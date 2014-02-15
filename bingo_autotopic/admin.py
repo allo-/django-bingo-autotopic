63b1bebed79a50d11fd4a62f190b8bae5d7ccc79
Alexander Schier <allo@laxu.de>
2014-02-15 14:57:18 +0100

    nicer admin interface for GameDescription model

--- bingo_autotopic/admin.py
+++ bingo_autotopic/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(GameDescription)
+
+class GameDescriptionAdmin(admin.ModelAdmin):
+    list_display = ("description", "start_time", "end_time", "site")
+    list_filter = ("site",)
+
+
+admin.site.register(GameDescription, GameDescriptionAdmin)
