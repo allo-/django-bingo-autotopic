2a252e6d1b0a3e5137e9411fd7a882e595a061fc
allo <allo@laxu.de>
2015-07-23 22:54:06 +0200

    added settings AUTOTOPIC_DEFAULT_START_TIME and AUTOTOPIC_DEFAULT_END_TIME

--- bingo_autotopic/models.py
+++ bingo_autotopic/models.py
@@ -2,18 +2,47 @@ from django.db import models
 from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from bingo.models import Game
 from bingo import times
 
+import datetime
+
+
+AUTOTOPIC_DEFAULT_START_TIME = getattr(
+    settings, "AUTOTOPIC_DEFAULT_START_TIME", None)
+AUTOTOPIC_DEFAULT_END_TIME = getattr(
+    settings, "AUTOTOPIC_DEFAULT_END_TIME", None)
+
+
+def default_start_time():
+    if AUTOTOPIC_DEFAULT_START_TIME is None:
+        return
+    now = datetime.datetime.now()
+    (hour, minute, second) = AUTOTOPIC_DEFAULT_START_TIME
+    now = now.replace(hour=hour or 0, minute=minute or 0, second=second or 0)
+    return now
+
+
+def default_end_time():
+    if AUTOTOPIC_DEFAULT_END_TIME is None:
+        return
+    now = datetime.datetime.now()
+    (hour, minute, second) = AUTOTOPIC_DEFAULT_END_TIME
+    now = now.replace(hour=hour or 0, minute=minute or 0, second=second or 0)
+    return now
+
 
 class GameDescription(models.Model):
     start_time = models.DateTimeField(
-        help_text=_("Game started after this time"))
+        help_text=_("Game started after this time"),
+        default=default_start_time)
     end_time = models.DateTimeField(
-        help_text=_("Game started before this time"))
+        help_text=_("Game started before this time"),
+        default=default_end_time)
     description = models.CharField(
         max_length=255,
         help_text=_("Description, which is added to matching games."))
@@ -37,7 +66,8 @@ class GameDescription(models.Model):
                 _("Interval overlaps with another description."))
 
     def __unicode__(self):
-       return u"Game Description for {start:s} (site: {site:s})".format(start=self.start_time.strftime("%Y-%m-%d %H:%M"), site=self.site)
+        return u"Game Description for {start:s} (site: {site:s})".format(
+            start=self.start_time.strftime("%Y-%m-%d %H:%M"), site=self.site)
 
 
 @receiver(pre_save, sender=Game)
